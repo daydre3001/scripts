4bb039f812450778577a015a9e2a5b9e6ee01828
Dreday <a.sanchez@sae.edu>
2021-11-04 13:43:51 -0400

    fixed issue with name of vol to mount

--- 8x8Install.sh
+++ 8x8Install.sh
@@ -35,7 +35,7 @@ OSvers_URL=$( sw_vers -productVersion | sed 's/[.]/_/g' )
 ## Set the User Agent string for use with curl
 userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X ${OSvers_URL}) AppleWebKit/535.6.2 (KHTML, like Gecko) Version/5.2 Safari/535.6.2"
 
-## Get the latest version of Reader available from 8x8 website.
+## Get the latest version of 8x8 work available from 8x8 website.
 url=`/usr/bin/curl -s -A "$userAgent" https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop | grep '.dmg' | awk -F'https://' '{print $2}' | awk -F'href' 'NR==1 {print $1}' | sed 's/"//'`
 imgfile=`echo ${url} | cut -d '/' -f3`
 
@@ -46,7 +46,7 @@ echo "`date` : Downloading 8x8 image" >> ${logfile}
 ## Attach and copy the 8x8 app
 echo "`date` : Attaching image" >> ${logfile}
 /usr/bin/hdiutil attach -nobrowse /tmp/${imgfile}
-installvol=$(ls /Volumes/ | grep 8x8)
+installvol=$(ls /Volumes/ | grep work)
 appName=`ls "/Volumes/${installvol}/" | grep 8x8`
 echo "`date` : Copying $appName to Applications folder"
 /bin/cp -R "/Volumes/${installvol}/${appName}" /Applications/
