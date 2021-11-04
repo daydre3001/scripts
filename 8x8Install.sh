55e368f510dbc4e108a221d1ba93f6007001420e
Dreday <a.sanchez@sae.edu>
2021-11-04 15:08:45 -0400

    Refactor and clean up for M1 use

--- 8x8Install.sh
+++ 8x8Install.sh
@@ -11,9 +11,7 @@
 #
 ####################################################################################################
 #
-# HISTORY
-#
-#       Version: 1
+#       Version: 1.5
 #
 #       - Andres Sanchez, 20.4.2021
 #       
@@ -22,12 +20,17 @@
 # Script to download and install/update 8x8 Work.
 #
 
+workPID=$(pgrep '8x8')
 logfile="/Library/Logs/8x8InstallScript.log"
 
-## Close 8x8
-pkill '^8x8 Work$'
-/bin/rm -r -f "/Applications/8x8 Work.app"
+## Close 8x8 if runnig
+if [ "${workPID}" != "" ]; then
+        kill ${workPID}
+        echo "`date` : closing zoom " >> ${logfile}
+fi
+
 echo "`date` : Deleting 8x8" >> ${logfile}
+/bin/rm -r -f "/Applications/8x8 Work.app"
 
 ## Get OS version and adjust for use with the URL string
 OSvers_URL=$( sw_vers -productVersion | sed 's/[.]/_/g' )
@@ -46,13 +49,13 @@ echo "`date` : Downloading 8x8 image" >> ${logfile}
 ## Attach and copy the 8x8 app
 echo "`date` : Attaching image" >> ${logfile}
 /usr/bin/hdiutil attach -nobrowse /tmp/${imgfile}
-installvol=$(ls /Volumes/ | grep work)
+installvol=$(ls /Volumes/ | grep 8x8)
 appName=`ls "/Volumes/${installvol}/" | grep 8x8`
-echo "`date` : Copying $appName to Applications folder"
+echo "`date` : Copying $appName to Applications folder" >> ${logfile}
 /bin/cp -R "/Volumes/${installvol}/${appName}" /Applications/
 
 ## Detach and remove image
-echo "`date` : Detaching and cleaning up" >> ${logfile}
+echo "`date` : Detaching $installvol and cleaning up" >> ${logfile}
 /usr/bin/hdiutil detach /Volumes/"${installvol}"
 /bin/rm /tmp/${imgfile}
 echo "`date` : 8x8 is ready" >> ${logfile}
