6690db435b5de5b39a7308d20ced79c8cd4f22fc
Dreday <a.sanchez@sae.edu>
2021-11-09 09:03:20 -0500

    Chrome install and update WIP

---
+++ ChromeInstall.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#####################################################################################################
+#
+# ABOUT THIS PROGRAM
+#
+# NAME
+#       ChromeInstall.sh -- Installs Google Chrome
+#
+# SYNOPSIS
+#       sudo ChromeInstall.sh
+#
+####################################################################################################
+#
+#       Version: 1.0
+#
+#       - Andres Sanchez, 8.11.2021
+#       
+#
+####################################################################################################
+# Script to download and install Google Chrome.
+
+logfile="/Library/Logs/ChromeInstall.log"
+imgfile="googlechrome.dmg"
+
+if  [[ `arch` == arm64 ]]; then
+        echo "Architecture is Apple ARM"
+        curl -L -s -S -o /tmp/${imgfile} https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg
+    else
+        echo "Architecture is Intel X86"
+        curl -L -s -S -o /tmp/${imgfile} https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+fi
+
+echo "Deleting old version of Chrome" >> ${logfile}
+rm -rf /Apllications/Google\ Chrome.app
+cho "Attaching and installing Chrome" >> ${logfile}
+/usr/bin/hdiutil attach /tmp/${imgfile}
+cp -R /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
+/bin/sleep 5
+echo "Cleaning up" >> ${logfile}
+/usr/bin/hdiutil detach /Volumes/Google\ Chrome/
+/bin/rm /tmp/${imgfile}
+/bin/sleep 5
+
+echo "Setting AutoUpdate for Google Chrome" >> ${logfile}
+python ChromeAutoUpdate.py
