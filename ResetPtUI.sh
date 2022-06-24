e73fe487f92a64ce0bf016931ddc8a145dd7d238
daydre3001 <a.sanchez@sae.edu>
2022-06-24 13:04:20 -0400

    Updated PT prefs/UI reset

--- ResetPtUI.sh
+++ ResetPtUI.sh
@@ -1,1 +1,4 @@
 rm /Users/student/Library/Preferences/Avid/Pro\ Tools/UICustomization.txt
+rm /Users/student/Library/Preferences/Avid/Pro\ Tools/UITheme.txt
+
+echo "Default" >> /Users/student/Library/Preferences/Avid/Pro\ Tools/UITheme.txt
