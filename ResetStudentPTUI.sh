d0fe1ec3a20a084762c2884a0cb21ee90457f896
Dreday <a.sanchez@sae.edu>
2022-08-03 10:28:23 -0400

    Minor updates to PTUI and ZoomInstall

--- ResetStudentPTUI.sh
+++ ResetStudentPTUI.sh
@@ -1,4 +1,4 @@
 rm /Users/student/Library/Preferences/Avid/Pro\ Tools/UICustomization.txt
 rm /Users/student/Library/Preferences/Avid/Pro\ Tools/UITheme.txt
 
-echo "Default" >> /Users/student/Library/Preferences/Avid/Pro\ Tools/UITheme.txt
+echo "Classic" >> /Users/student/Library/Preferences/Avid/Pro\ Tools/UITheme.txt
